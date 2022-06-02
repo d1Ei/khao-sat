a3475a6c36944e21ca8689fa7a580ef472791654
Pham An <an9301hv17@gmail.com>
2022-06-02 22:27:23 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Khảo sát',
+    introDesc: `Anh có điều này muốn hỏi, em trả lời thật lòng nhé`,
+    btnIntro: 'Ỏ kii',
+    title: 'Em biết anh thích em mà đúng không?',
+    desc: 'Vậy em có tình cảm gì với anh không, dù chỉ một chút ? ',
+    btnYes: 'Em cũng thích anh áaaa',
+    btnNo: 'Em khôngg',
+    question:'Em nói thật không á ??',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Anh code chỗ này là dù em nhập gì thì nó cũng ra dòng này :)) ',
+    mess: 'Anh thích em thật lòng á. Thích vãoooo',
+    messDesc: 'À nếu câu trả lời của em là không thì em chỉ cần nhắn :> là anh hiểu r',
+    btnAccept: 'Ỏ kii',
+    messLink: 'https://www.instagram.com/d1ei._.darealest/'
+}
